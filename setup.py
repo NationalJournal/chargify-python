cf6910c83e817a06ec32155fdffe6acc00621f5f
Ben Tappin <ben@mrben.co.uk>
2017-06-16 16:13:19 +0100

    Add a setup.py.

---
+++ setup.py
@@ -0,0 +1,41 @@
+# encoding: utf-8
+from setuptools import setup
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+
+requirements = [
+    'requests<3.0.0',
+]
+
+
+setup(
+    name='chargify-python',
+    version='0.0.1',
+    long_description=readme,
+    url='https://github.com/code-kitchen/chargify-python/',
+    license=license,
+    install_requires=requirements,
+    packages=['.'],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Utilities',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    )
+)
