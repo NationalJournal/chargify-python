bffb290ec94ac783f108cf3ab15501133ea6d3bf
Ben Tappin <ben@mrben.co.uk>
2017-06-18 08:56:10 +0100

    No need to repeat host everywhere in tests.

--- test_chargify.py
+++ test_chargify.py
@@ -12,8 +12,10 @@ class ChargifyHttpClientStub(object):
 class ChargifyTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.chargify = Chargify('api_key', 'subdomain',
+        subdomain = 'subdomain'
+        self.chargify = Chargify('api_key', subdomain,
                                  client=ChargifyHttpClientStub())
+        self.host = 'https://%s.chargify.com' % subdomain
 
     def assertResult(self, result, expected_url, expected_method,
                      expected_params, expected_data):
@@ -22,7 +24,7 @@ class ChargifyTestCase(unittest.TestCase):
         method, and POST data are being constructed from the Chargify API.
         """
         url, method, params, data = result
-        self.assertEqual(url, expected_url)
+        self.assertEqual(url, '%s%s' % (self.host, expected_url))
         self.assertEqual(method, expected_method)
         self.assertEqual(params, expected_params)
         self.assertEqual(data, expected_data)
@@ -35,7 +37,7 @@ class TestCustomers(ChargifyTestCase):
         result = self.chargify.customers()
         self.assertResult(
             result,
-            'https://subdomain.chargify.com/customers.json',
+            '/customers.json',
             'GET',
             {},
             {}
@@ -45,7 +47,7 @@ class TestCustomers(ChargifyTestCase):
         result = self.chargify.customers(customer_id=123)
         self.assertResult(
             result,
-            'https://subdomain.chargify.com/customers/123.json',
+            '/customers/123.json',
             'GET',
             {},
             {}
@@ -55,7 +57,7 @@ class TestCustomers(ChargifyTestCase):
         result = self.chargify.customers.lookup(reference=123)
         self.assertResult(
             result,
-            'https://subdomain.chargify.com/customers/lookup.json',
+            '/customers/lookup.json',
             'GET',
             {'reference': 123},
             {}
@@ -73,7 +75,7 @@ class TestCustomers(ChargifyTestCase):
         )
         self.assertResult(
             result,
-            'https://subdomain.chargify.com/customers.json',
+            '/customers.json',
             'POST',
             {},
             {'customer': {'first_name': 'Joe', 'last_name': 'Blow', 'email': 'joe@example.com'}}
@@ -90,7 +92,7 @@ class TestCustomers(ChargifyTestCase):
         )
         self.assertResult(
             result,
-            'https://subdomain.chargify.com/customers/123.json',
+            '/customers/123.json',
             'PUT',
             {},
             {'customer': {'email': 'joe@example.com'}}
@@ -100,7 +102,7 @@ class TestCustomers(ChargifyTestCase):
         result = self.chargify.customers.delete(customer_id=123)
         self.assertResult(
             result,
-            'https://subdomain.chargify.com/customers/123.json',
+            '/customers/123.json',
             'DELETE',
             {},
             {}
@@ -114,7 +116,7 @@ class TestProducts(ChargifyTestCase):
         result = self.chargify.products()
         self.assertResult(
             result,
-            'https://subdomain.chargify.com/products.json',
+            '/products.json',
             'GET',
             {},
             {}
@@ -124,7 +126,7 @@ class TestProducts(ChargifyTestCase):
         result = self.chargify.products(product_id=123)
         self.assertResult(
             result,
-            'https://subdomain.chargify.com/products/123.json',
+            '/products/123.json',
             'GET',
             {},
             {}
@@ -134,7 +136,7 @@ class TestProducts(ChargifyTestCase):
         result = self.chargify.products.handle(handle='myhandle')
         self.assertResult(
             result,
-            'https://subdomain.chargify.com/products/handle/myhandle.json',
+            '/products/handle/myhandle.json',
             'GET',
             {},
             {}
@@ -148,7 +150,7 @@ class TestSubscriptions(ChargifyTestCase):
         result = self.chargify.customers.subscriptions(customer_id=123)
         self.assertResult(
             result,
-            'https://subdomain.chargify.com/customers/123/subscriptions.json',
+            '/customers/123/subscriptions.json',
             'GET',
             {},
             {}
@@ -158,7 +160,7 @@ class TestSubscriptions(ChargifyTestCase):
         result = self.chargify.subscriptions(subscription_id=123)
         self.assertResult(
             result,
-            'https://subdomain.chargify.com/subscriptions/123.json',
+            '/subscriptions/123.json',
             'GET',
             {},
             {}
@@ -184,7 +186,7 @@ class TestSubscriptions(ChargifyTestCase):
         )
         self.assertResult(
             result,
-            'https://subdomain.chargify.com/subscriptions.json',
+            '/subscriptions.json',
             'POST',
             {},
             {'subscription': {'product_handle': 'my_product', 'credit_card_attributes': {'expiration_month': '10', 'full_number': '1', 'expiration_year': '2020'}, 'customer_attributes': {'first_name': 'Joe', 'last_name': 'Blow', 'email': 'joe@example.com'}}}
@@ -205,7 +207,7 @@ class TestSubscriptions(ChargifyTestCase):
         )
         self.assertResult(
             result,
-            'https://subdomain.chargify.com/subscriptions/123.json',
+            '/subscriptions/123.json',
             'PUT',
             {},
             {'subscription': {'credit_card_attributes': {'expiration_month': '10', 'full_number': '2', 'expiration_year': '2030'}}}
@@ -222,7 +224,7 @@ class TestSubscriptions(ChargifyTestCase):
         )
         self.assertResult(
             result,
-            'https://subdomain.chargify.com/subscriptions/123.json',
+            '/subscriptions/123.json',
             'DELETE',
             {},
             {'subscription': {'cancellation_message': 'Goodbye!'}}
@@ -244,7 +246,7 @@ class TestCharges(ChargifyTestCase):
         )
         self.assertResult(
             result,
-            'https://subdomain.chargify.com/subscriptions/123/charges.json',
+            '/subscriptions/123/charges.json',
             'POST',
             {},
             {'charge': {'amount': '1.00', 'memo': 'This is the description of the one time charge.'}}
@@ -259,7 +261,7 @@ class TestComponents(ChargifyTestCase):
             subscription_id=123, component_id=456)
         self.assertResult(
             result,
-            'https://subdomain.chargify.com/subscriptions/123/components/456/usages.json',
+            '/subscriptions/123/components/456/usages.json',
             'GET',
             {},
             {}
@@ -278,7 +280,7 @@ class TestComponents(ChargifyTestCase):
         )
         self.assertResult(
             result,
-            'https://subdomain.chargify.com/subscriptions/123/components/456/usages.json',
+            '/subscriptions/123/components/456/usages.json',
             'POST',
             {},
             {'usage': {'memo': 'My memo', 'quantity': 5}}
@@ -297,7 +299,7 @@ class TestMigrations(ChargifyTestCase):
         )
         self.assertResult(
             result,
-            'https://subdomain.chargify.com/subscriptions/123/migrations.json',
+            '/subscriptions/123/migrations.json',
             'POST',
             {},
             {'product_id': 1234}
@@ -311,7 +313,7 @@ class TestReactivate(ChargifyTestCase):
         result = self.chargify.subscriptions.reactivate.update(subscription_id=123)
         self.assertResult(
             result,
-            'https://subdomain.chargify.com/subscriptions/123/reactivate.json',
+            '/subscriptions/123/reactivate.json',
             'PUT',
             {},
             {}
@@ -325,7 +327,7 @@ class TestTransactions(ChargifyTestCase):
         result = self.chargify.transactions()
         self.assertResult(
             result,
-            'https://subdomain.chargify.com/transactions.json',
+            '/transactions.json',
             'GET',
             {},
             {}
@@ -335,7 +337,7 @@ class TestTransactions(ChargifyTestCase):
         result = self.chargify.subscriptions.transactions(subscription_id=123)
         self.assertResult(
             result,
-            'https://subdomain.chargify.com/subscriptions/123/transactions.json',
+            '/subscriptions/123/transactions.json',
             'GET',
             {},
             {}
